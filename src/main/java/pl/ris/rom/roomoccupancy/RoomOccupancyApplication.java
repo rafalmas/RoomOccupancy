08165abce2c88dbc94e5a7e6248d604e2b7d0fa5
Rafal <rafal.maslana@gmail.com>
2022-10-19 17:13:59 +0200

    Initial project skeleton

---
+++ src/main/java/pl/ris/rom/roomoccupancy/RoomOccupancyApplication.java
@@ -0,0 +1,13 @@
+package pl.ris.rom.roomoccupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ris/rom/roomoccupancy/RoomOccupancyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ris.rom.roomoccupancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
